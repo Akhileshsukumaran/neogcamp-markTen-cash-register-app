59431eeecd1a82d250147ecc2068f64a821792e6
Akhileshsukumaran <akhileshs2017@gmail.com>
2021-08-27 17:27:44 +0530

    script and style updated

--- app.js
+++ app.js
@@ -1,36 +1,78 @@
 var billAmount = document.querySelector("#bill-amount");
 var cashGiven = document.querySelector("#cash-given");
+var lblCashGiven = document.querySelector(".cash-given");
 var checkButton = document.querySelector("#btn-check");
+var nextButton = document.querySelector("#btn-next");
 var errorMsg = document.querySelector("#error-message");
 var noOfNotes = document.querySelectorAll(".no-of-notes");
 var availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
-checkButton.addEventListener("click", function checkBillandCashAmount(){
+hideMsg();
 
-   hideMsg();
+nextButton.addEventListener("click", function checkBillAmount(){
 
-    if(billAmount.value > 0)
+    if(billAmount.value > 0 && isNaN(billAmount.value) === false)
     {
-        if(cashGiven.value >= billAmount.value)
-        {
-            const amountToBeReturned = cashGiven.value - billAmount.value;
-            CalculateChange(amountToBeReturned);
-
-        }
-        else
-        {
-            showError("Do you want to wash plates?");
-        }
+        hideMsg();
+        ShowCashGiven();
+        CheckChange();
+    }
+    else if (isNaN(billAmount.value))
+    {
+        showError("Please enter a valid amount!");
     }
     else
     {
        showError("Please enter an amount greater than 0 !");
     }
+
 })
+
+function CheckChange()
+{
+    checkButton.addEventListener("click", function checkBillandCashAmount(){
+     
+         if(billAmount.value > 0)
+         {
+             ShowCashGiven();
+     
+             if(cashGiven.value >= billAmount.value)
+             {
+                 hideMsg();
+                 ShowCashGiven()
+                 const amountToBeReturned = cashGiven.value - billAmount.value;
+                 CalculateChange(amountToBeReturned);
+     
+             }
+             else
+             {
+                 showError("Do you want to wash plates?");
+             }
+         }
+         else if ( typeof billAmount.value === 'String')
+         {
+             showError("Please enter a valid amount!");
+         }
+         else
+         {
+            showError("Please enter an amount greater than 0 !");
+         }
+     })
+
+}
+
+
+function ShowCashGiven()
+{
+    cashGiven.style.visibility = "visible";
+    lblCashGiven.style.visibility = "visible";
+}
 
 function hideMsg()
 {
     errorMsg.style.display = "none";
+    cashGiven.style.visibility = "hidden";
+    lblCashGiven.style.visibility = "hidden";
 }
 
 function CalculateChange(amountToBeReturned)
